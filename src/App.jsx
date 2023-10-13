f6dae9c705be518893017591719270981b3c5e14
Himanshu <kyadav.9643@gmail.com>
2023-10-13 15:06:01 +0530

    added required paths
    adding paths in header as components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
 import {Home} from "./pages/Home";
+import {Profile} from "./pages/Profile";
+import {Login} from "./pages/Login";
+import {Register} from "./pages/Register";
 import { Header } from './components/Header';
+import axios from "axios";
+import {Context,server} from './main';
 
 function App() {
   
@@ -9,6 +14,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home/>}  />
+        <Route path="/profile" element={<Profile/>}  />
+        <Route path="/login" element={<Login/>}  />
+        <Route path="/Register" element={<Register/>}  />
       </Routes>
     </Router>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useContext } from 'react'
+import { toast } from 'react-hot-toast';
+import { Context, server } from '../main';
 import { Link } from 'react-router-dom'
 
 export const Header = () => {
-  return <nav className='header'>
-        <div>
-            <h2>Todo App.</h2>
-        </div>
-        <article>
-            <Link to={"/"}> Home</Link>
-        </article>
-    </nav>
+    const { isAuthenticated, setIsAuthenticated, loading, setLoading } = useContext(Context);
+    const logoutHandler = async () => {
+        setLoading(true);
+        try {
+            await axios.get(`${server}/users/logout`, { withCredentials: true, });
+            toast.success("Logged out Successfully");
+            setIsAuthenticated(false);
+            setLoading(false);
+        } catch (error) {
+            toast.error(error.response.data.message);
+            setIsAuthenticated(true);
+            setLoading(false);
+        }
+    };
+
+    return (
+        <nav className='header'>
+            <div>
+                <h2>Todo App.</h2>
+            </div>
+            <article>
+                <Link to={"/"}> Home</Link>
+                <Link to={"/profile"}> Profile</Link>
+                {isAuthenticated ? (
+                    <button disabled={loading} onClick={logoutHandler} className='btn'>Logout</button>)
+                    : (<button className='btn'>Login</button>
+                    )}
+            </article>
+        </nav>);
 }
